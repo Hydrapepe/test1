e560e99ccdd3341e6cc5afecb6c42ccc0a3b7992
Hydrapepe <maus3212@gmail.com>
2020-06-04 10:32:34 +0300

    add Split

--- PepeForWinS/Form11.Designer.cs
+++ PepeForWinS/Form11.Designer.cs
@@ -29,7 +29,14 @@
         private void InitializeComponent()
         {
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button3
@@ -42,19 +49,94 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
-            // textBox8
+            // textBox1
             // 
-            this.textBox8.Location = new System.Drawing.Point(349, 215);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(102, 20);
-            this.textBox8.TabIndex = 10;
+            this.textBox1.Location = new System.Drawing.Point(628, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(102, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(625, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Количество пользователей";
+            this.label1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(526, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(276, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Нажми если нужно больше одного пользователя";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Группа";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(260, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Нажми если нужно создать отдельную группу";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Домен";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(102, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Костыль";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form11
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Name = "Form11";
             this.Text = "Создание пользователей";
@@ -65,6 +147,13 @@
 
         #endregion
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- PepeForWinS/Form11.cs
+++ PepeForWinS/Form11.cs
@@ -12,6 +12,10 @@ namespace PepeForWinS
 {
     public partial class Form11 : Form
     {
+            
+        public string ii = "$RootDomain = \"DC=savilltech,DC=net\"\n$OUs = Get-ADOrganizationalUnit -filter* -searchbase \"$RootDomain\" -SearchScope OneLevel\nforeach($OU in $OUs)\n{\n$GroupOU = \"OU=Groups,$($OU.DistinguishedName)\"\n$UserOU = \"OU=Users,$($OU.DistinguishedName)\"\n$NewGroupName = \"$($OU.Name)Users\"\n$NewGroup = New-ADGroup -Name $NewGroupName -GroupCategory Security -GroupScope Global -DisplayName $NewGroupName -Path $GroupOU -Description \"$($OU.Name) Users Group\" -PassThru\n$Users = Get-ADUser -Filter* -SearchBase $UserOU\nforeach($User in $Users)\n{\n$User | Add-ADPrincipalGroupMembership -MemberOf $NewGroupName\n}\n};";
+        public string test;
+        public string test1;
         public Form11()
         {
             InitializeComponent();
@@ -21,5 +25,40 @@ namespace PepeForWinS
         {
             Application.Exit();
         }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            test = textBox2.Text;
+            string[] words = test.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                try
+                {
+                    words[0] = "DC=" + words[0];
+                    words[1] = "DC=" + words[1];
+                    words[2] = "DC=" + words[2];
+                    words[3] = "DC=" + words[3];
+                    words[4] = "DC=" + words[4];
+                    words[5] = "DC=" + words[5];
+                }
+                catch
+                { }
+                test1 = "$RootDomain = \"" + words[0] + "\"";
+                test1 = test1 + "\"" + words[1] + "\"";
+                test1 = test1 + "\"" + words[2] + "\"";
+                test1 = test1 + "\"" + words[3] + "\"";
+                test1 = test1 + "\"" + words[4] + "\"";
+                test1 = test1 + "\"" + words[5] + "\"";
+            }
+            catch { label2.Text = test1; }
+        }
     }
 }
+
+
