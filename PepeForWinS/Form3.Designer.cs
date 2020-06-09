2c1769a236f49ee16124e29fb4d8de041af7c08d
Hydrapepe <maus3212@gmail.com>
2020-06-09 21:47:41 +0300

    split

--- PepeForWinS/Form3.Designer.cs
+++ PepeForWinS/Form3.Designer.cs
@@ -44,8 +44,6 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -135,7 +133,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(89, 150);
+            this.label5.Location = new System.Drawing.Point(145, 150);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 13);
             this.label5.TabIndex = 8;
@@ -143,7 +141,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(67, 176);
+            this.textBox5.Location = new System.Drawing.Point(123, 176);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(102, 20);
             this.textBox5.TabIndex = 7;
@@ -151,7 +149,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(211, 150);
+            this.label6.Location = new System.Drawing.Point(267, 150);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 13);
             this.label6.TabIndex = 10;
@@ -159,14 +157,14 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(191, 176);
+            this.textBox6.Location = new System.Drawing.Point(247, 176);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(102, 20);
             this.textBox6.TabIndex = 9;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(316, 176);
+            this.textBox7.Location = new System.Drawing.Point(372, 176);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(102, 20);
             this.textBox7.TabIndex = 9;
@@ -174,27 +172,11 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(336, 150);
+            this.label7.Location = new System.Drawing.Point(392, 150);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(58, 13);
             this.label7.TabIndex = 10;
             this.label7.Text = "User name";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(436, 176);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(102, 20);
-            this.textBox8.TabIndex = 9;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(425, 150);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(180, 13);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Password(число+ высший регистр)";
             // 
             // Form3
             // 
@@ -202,10 +184,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(617, 301);
             this.ControlBox = false;
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label5);
@@ -246,7 +226,5 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Label label8;
     }
 }
