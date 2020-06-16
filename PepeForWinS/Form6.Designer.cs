88058e50cadc7bb5e70aa29dbac65529464191a9
Hydrapepe <maus3212@gmail.com>
2020-06-16 19:05:25 +0300

    pepe

--- PepeForWinS/Form6.Designer.cs
+++ PepeForWinS/Form6.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.Выход = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,7 +38,7 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,15 +51,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // Выход
+            // button2
             // 
-            this.Выход.Location = new System.Drawing.Point(12, 415);
-            this.Выход.Name = "Выход";
-            this.Выход.Size = new System.Drawing.Size(75, 23);
-            this.Выход.TabIndex = 1;
-            this.Выход.Text = "Выход";
-            this.Выход.UseVisualStyleBackColor = true;
-            this.Выход.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.Location = new System.Drawing.Point(12, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // label1
             // 
@@ -118,6 +118,7 @@
             this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "Уровень ограничения юзер";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
             // 
             // checkBox2
             // 
@@ -129,16 +130,16 @@
             this.checkBox2.Text = "Уровень ограничения Администратор";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // button3
             // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(667, 397);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 41);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Генерация";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(667, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Генерация";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form6
             // 
@@ -146,7 +147,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox3);
@@ -155,7 +156,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Выход);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form6";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -168,7 +169,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button Выход;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
@@ -177,6 +178,6 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- PepeForWinS/Form6.cs
+++ PepeForWinS/Form6.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO.Pipes;
 using System.Windows.Forms;
+using System.Text;
+using System.IO;
 
 namespace PepeForWinS
 {
@@ -9,6 +12,8 @@ namespace PepeForWinS
         {
             InitializeComponent();
         }
+        public int i;
+        public string NAME_POLISY,text,DOMAIN_NAME_FULL, NAME_GROUP,SERVER_DOT_SPLIT;
 
         private void Button1_Click(object sender, EventArgs e)
         {
@@ -18,6 +23,58 @@ namespace PepeForWinS
         private void Button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            i = 1 ;
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (i == 1)
+            {
+                DOMAIN_NAME_FULL = textBox3.Text;
+                NAME_POLISY = textBox1.Text;
+                NAME_GROUP = textBox2.Text;
+
+                string[] words = DOMAIN_NAME_FULL.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    words[0] = "OU="+NAME_GROUP+ ","+"DC=" + words[0];
+                    SERVER_DOT_SPLIT = words[0];
+                    words[1] = "," + "DC=" + words[1];
+                    SERVER_DOT_SPLIT += words[1];
+                    words[2] = "," + "DC=" + words[2];
+                    SERVER_DOT_SPLIT += words[2];
+                    words[3] = "," + "DC=" + words[3];
+                    SERVER_DOT_SPLIT += words[3];
+                    words[4] = "," + "DC=" + words[4];
+                    SERVER_DOT_SPLIT += words[4];
+                    words[5] = "," + "DC=" + words[5];
+                    SERVER_DOT_SPLIT += words[5];
+                }
+                catch 
+                {
+                    text = "New-GPO -Name \"" + NAME_POLISY + "\" | New-GPLink -Target \""+SERVER_DOT_SPLIT+"\"\nSet-GPRegistryValue -Name \"" + NAME_POLISY + "\" -Key \"HKCU\\Software\\Policies\\Microsoft\\Windows\\System\" -ValueName DisableCMD -Type DWord -Value 1\nSet-GPRegistryValue -Name \"" + NAME_POLISY + "\" -Key \"HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\" -ValueName NoRun -Type DWord -Value 1\nSet-GPRegistryValue -Name \"" + NAME_POLISY + "\" -Key \"HKCU\\Software\\Policies\\Microsoft\\CurrentVersion\\Policies\\System\" -ValueName DisableRegistryTools -Type DWord -Value 1\nSet-GPRegistryValue -Name \"" + NAME_POLISY + "\" -Key \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\" -ValueName NoControlPanel -Type DWord -Value 1\nSet-GPRegistryValue -Name \"" + NAME_POLISY + "\" -Key \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\" -ValueName ScreenSaveActive -Type String -Value 0\nSet-GPRegistryValue -Name \"" + NAME_POLISY + "\" -Key \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\ActiveDesktop\" -ValueName NoChangingWallPaper -Type DWord -Value 1";
+                    using (FileStream fs = File.Create("polisy.ps1"))
+                    {
+                        Encoding win1251 = Encoding.GetEncoding(1251);
+                        string info = UTF8ToWin1251(text);
+                        using (var sr = new StreamWriter(fs, win1251))
+                        {
+                            sr.Write(info);
+                        }
+                    }
+                }
+            }
+        }
+        private static string UTF8ToWin1251(string sourceStr)
+        {
+            Encoding utf8 = Encoding.UTF8;
+            Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+            byte[] utf8Bytes = utf8.GetBytes(sourceStr);
+            byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
+            return win1251.GetString(win1251Bytes);
         }
     }
 }
